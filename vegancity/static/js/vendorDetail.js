b21d3de29a0212ac9138b8a75176e0e2db9c4cfc
steve <steventlamb@gmail.com>
2014-05-04 19:57:07 -0400

    fixup vendor_detail

--- vegancity/static/js/vendorDetail.js
+++ vegancity/static/js/vendorDetail.js
@@ -5,15 +5,15 @@ var VEGANCITY = VEGANCITY || {};
 (function (N) {
     N.vendorDetail = N.vendorDetail || {};
 
-    N.vendorDetail.initVendorDetailMap = function (name, lat, lng) {
+    N.vendorDetail.initMap = function (name, lat, lng) {
         var vendorLocation = [lat, lng];
 
-        var map = L.map("map_canvas").setView([lat, lng], 17);
+        var map = L.map("map_canvas").setView(vendorLocation, 17);
 
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([lat, lng]).addTo(map).bindPopup(name).openPopup();
+        L.marker(vendorLocation).addTo(map).bindPopup(name).openPopup();
     };
 })(VEGANCITY);
