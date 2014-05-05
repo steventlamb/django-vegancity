4af558e9bbdf868061f17d1e6fd02016846f052f
steve <steventlamb@gmail.com>
2014-05-04 20:17:17 -0400

    Acked-by: steve <steventlamb@gmail.com>

---
+++ vegancity/static/js/app.js
@@ -0,0 +1,12 @@
+/*global $, L */
+
+// The parent of all javascript
+
+var VEGANCITY = {
+    vendors: {},
+    vendorDetail: {},
+    mapUtils: {
+        TILE_URL: 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg',
+        TILE_ATTRIBUTION: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }
+};
